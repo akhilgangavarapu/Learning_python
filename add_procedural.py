32d1b974ecaf1f06a746e921f5699047e22f1f0a
akhil <akhil.gangavarapu@gmail.com>
2022-10-04 16:28:36 +1100

    This is my first add procedure

---
+++ add_procedural.py
@@ -0,0 +1,16 @@
+#get the first number
+x=2
+#get the second number
+y=3
+#add two numbers abd store the result
+z=x+y
+#print(z)
+print(z)
+#get the first number
+x=18
+#get the second number
+y=2
+#add two numbers abd store the result
+z=x+y
+#print(z)
+print(z)
