2761bcd9ebc574f272538d31a20468175cfeec42
akhil <akhil.gangavarapu@gmail.com>
2022-10-06 18:10:33 +1100

    This is my first multiplication procedure

---
+++ multiplication_procedural.py
@@ -0,0 +1,16 @@
+#get the first number
+x=4
+#get the second number
+y=2
+#multiply two numbers divide store the result
+z=x*y
+#print(z)
+print(z)
+#get the first number
+x=18
+#get the second number
+y=2
+# multiply two numbers abd store the result
+z=x*y
+#print(z)
+print(z)
