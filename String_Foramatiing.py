a4268bc16b2b0aaf13e2251744d19b6a088f96cf
akhil <akhil.gangavarapu@gmail.com>
2022-10-27 20:28:25 +1100

    String Formatting Operations

--- String_Foramatiing.py
+++ String_Foramatiing.py
@@ -4,3 +4,16 @@ age = 25
 print("age =%d" %age)
 marks = 55
 print("marks=%f" %marks)
+
+name = 'Shaun'
+age = 32
+string = 'Hi, %s' %name
+print(string)
+
+string = '%s is %d years old' %(name,age)
+print(string)
+
+x = 'dance'
+print("Nancy can %s and %s too!" %(x, 'sing'))
+
+print ("Welcome to {} python tutorial." .format("ment360"))
